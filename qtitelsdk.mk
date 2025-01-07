3d0d0ebe86b6251b2ad25a822654f291aeb1b38e
Pratyush Gupta <quic_pragup@quicinc.com>
2025-01-07 19:29:04 +0530

    meta-qti-telsdk: Enable telux-public and telux-sampleApp Makefile
    Change-Id: I2b337cc73bdfd54c1be8b4755355a81fd7878653

---
+++ qtitelsdk.mk
@@ -0,0 +1,12 @@
+include $(INCLUDE_DIR)/target.mk
+
+QTITELSDK:=
+
+###Add target specific packages
+ifeq ($(BOARD),sdx35)
+	QTITELSDK+=
+else ifeq ($(BOARD),sdx65)
+	QTIDATAPROP+=
+else ifeq ($(BOARD),sdx85)
+    QTITELSDK+= telux-public
+endif
