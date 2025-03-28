049c7c332b70db7cf7f64b73254fe21e849c48c3
Ruixuan Gu <quic_ruixgu@quicinc.com>
2025-03-28 13:45:49 +0800

    meta-qti-telsdk: Enable wlan_test_app
    Change-Id: I1c4c36ce7c1026b039867ef56cdd6a7b9b2bce05

--- qtitelsdk.mk
+++ qtitelsdk.mk
@@ -8,5 +8,5 @@ ifeq ($(BOARD),sdx35)
 else ifeq ($(BOARD),sdx65)
 	QTIDATAPROP+=
 else ifeq ($(BOARD),sdx85)
-    QTITELSDK+= telux-public telsdk_console_app telux-DataApp telux-LocationTestApp telux-SwUpdateConsoleApp telux-SwUpdateApp telux-cellular_connection_security_app telux-wifi_connection_security_app telux-crypto_console_app
+    QTITELSDK+= telux-public telsdk_console_app telux-DataApp telux-LocationTestApp telux-SwUpdateConsoleApp telux-SwUpdateApp telux-cellular_connection_security_app telux-wifi_connection_security_app telux-crypto_console_app wlan_test_app
 endif
